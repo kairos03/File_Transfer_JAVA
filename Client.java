75a2fd783c039b85033b83117518564fedc98caa
kairos96 <kairos9603@gmail.com>
2015-08-19 14:33:18 +0900

    fist test

---
+++ Client.java
@@ -0,0 +1,46 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ConnectException;
+import java.net.Socket;
+ 
+ 
+public class Client 
+{
+	final static String serverIP = "127.0.0.1";
+	
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            System.out.println("Connecting. Server IP : " + serverIP);
+             
+            // socket Created and connect request
+            Socket socket = new Socket(serverIP, 5000);
+             
+            // get sockte's output stream
+            InputStream in = socket.getInputStream();
+            DataInputStream dis = new DataInputStream(in);  // 기본형 단위로 처리하는 보조스트림
+             
+            // data output
+            System.out.println("Message from server : " + dis.readUTF());
+            System.out.println("Connect close");
+             
+            // close socket, stream
+            dis.close();
+            socket.close();
+        } 
+        catch (ConnectException ce) 
+        {
+            ce.printStackTrace();
+        } 
+        catch (IOException ie) 
+        {
+            ie.printStackTrace();
+        } 
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        } 
+    } 
+} 
---
+++ Server.java
@@ -0,0 +1,64 @@
+// Server.java
+ 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Server 
+{
+	
+	//Define constant
+	final static int PORT = 5000;
+	
+    public static void main(String[] args) 
+    {
+        ServerSocket serverSocket = null;
+         
+        try 
+        {
+            // server Socket create and bind
+            serverSocket = new ServerSocket(PORT);
+            System.out.println(getTime() + " Listening...");
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        } // try - catch
+         
+        while (true) 
+        {
+            try 
+            {
+                // accept
+                Socket socket = serverSocket.accept();
+                System.out.println(getTime() + "Incoming conntect" + socket.getInetAddress() );
+                 
+                // open socket output stream
+                OutputStream out = socket.getOutputStream(); 
+                DataOutputStream dos = new DataOutputStream(out); // 기본형 단위로 처리하는 보조스트림
+                 
+                // Send data to remote socket
+                dos.writeUTF("Message from server.");
+                System.out.println(getTime() + " Data Transfered");
+                 
+                // closet stream and socket
+                dos.close();
+                socket.close();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            } // try - catch
+        } // while
+    } // main
+     
+    static String getTime() {
+        SimpleDateFormat f = new SimpleDateFormat("[hh:mm:ss]");
+        return f.format(new Date());
+    } // getTime
+} // TcpServerTest
+ 
